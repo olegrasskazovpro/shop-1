c5c50400bc53882aed978970e39297615389f5d7
OlegRasskazov <info@olegrasskazov.pro>
2018-08-21 22:55:50 +0300

    carousel script init added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
 "use strict";
 
-// тут будет код
+class Slider {
+  constructor () {
+
+  }
+
+  init() {
+
+  }
+}
+
+
+(function ($) {
+  $(function () {
+    // let slider = new Slider();
+    // slider.init();
+    $('.jcarousel').jcarousel({
+      wrap: 'circular'
+    });
+    $('.jcarousel-prev').click(function() {
+      $('.jcarousel').jcarousel('scroll', '-=1');
+    });
+
+    $('.jcarousel-next').click(function() {
+      $('.jcarousel').jcarousel('scroll', '+=1');
+    });
+  })
+})(jQuery);
