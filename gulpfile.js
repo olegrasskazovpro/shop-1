732594501b6815547ee01a2a6e9fe5a6f37c34a3
OlegRasskazov <info@olegrasskazov.pro>
2018-08-24 21:58:38 +0300

    required, path, config, html build task
    all installed plugins connected, path to src and dist added, config for dev server added

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+"use strict";
+
+let gulp = require('gulp');
+let browserSync = require('browser-sync');
+let prefixer = require('gulp-autoprefixer');
+let uglify = require('gulp-uglify');
+let sass = require('gulp-sass');
+let sourcemaps = require('gulp-sourcemaps');
+let rigger = require('gulp-rigger');
+let cssmin = require('gulp-minify-css');
+let tinify = require('tinify');
+let reload = browserSync.reload;
+
+// объект в который пропишем все нужные нам пути
+var path = {
+  dist: { //Тут мы укажем куда складывать готовые после сборки файлы
+    html: 'dist/',
+    js: 'dist/js/',
+    css: 'dist/css/',
+    img: 'dist/img/',
+    fonts: 'dist/fonts/'
+  },
+  src: { //Пути откуда брать исходники
+    html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+    js: 'src/js/main.js', //В стилях и скриптах нам понадобятся только main файлы
+    css: 'src/style/main.scss',
+    img: 'src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+    fonts: 'src/fonts/**/*.*'
+  },
+  watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+    html: 'src/**/*.html',
+    js: 'src/js/**/*.js',
+    css: 'src/css/**/*.scss',
+    img: 'src/img/**/*.*',
+    fonts: 'src/fonts/**/*.*'
+  },
+  clean: './build'
+};
+
+// Создадим переменную с настройками нашего dev сервера
+let config = {
+  server: {
+    baseDir: "./build"
+  },
+  tunnel: true,
+  host: 'localhost',
+  port: 9000,
+  logPrefix: "Frontend_Devil"
+};
