31b63adfe5179818ebb3071a2d3f2708107cafbc
OlegRasskazov <info@olegrasskazov.pro>
2018-08-28 13:47:26 +0300

    deploy json files with FTP, uglify commented

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 const gulp = require('gulp');
 const pump = require('pump');
 const prefixer = require('gulp-autoprefixer');
-const uglify = require('gulp-uglify');
+// const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const rigger = require('gulp-rigger');
@@ -12,6 +12,8 @@ const imagemin = require('gulp-imagemin');
 const rimraf = require('rimraf');
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
+const ftp = require('vinyl-ftp');
+const log = require('fancy-log');
 
 // объект в который пропишем все нужные нам пути
 let path = {
@@ -20,21 +22,27 @@ let path = {
     js: 'dist/js/',
     css: 'dist/css/',
     img: 'dist/img/',
-    fonts: 'dist/fonts/'
+    fonts: 'dist/fonts/',
+    db: 'dist/db/',
   },
   src: { //Пути откуда брать исходники
     html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
     js: 'src/js/main.js', //В стилях и скриптах нам понадобятся только main файлы
     css: 'src/css/**/style.sass',
     img: 'src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
-    fonts: 'src/fonts/**/*.*'
+    fonts: 'src/fonts/**/*.*',
+    db: 'src/db/*.json',
+  },
+  deploy: {
+    db: '*.json',
   },
   watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
     html: 'src/**/*.html',
     js: 'src/js/**/*.js',
     css: 'src/css/**/*.sass',
     img: 'src/img/**/*.*',
-    fonts: 'src/fonts/**/*.*'
+    fonts: 'src/fonts/**/*.*',
+    db: 'src/db/*.json'
   },
   clean: './dist'
 };
@@ -64,7 +72,7 @@ gulp.task('js:build', function (cb) {
       gulp.src(path.src.js), //Найдем наш main файл
       rigger(), //Прогоним через rigger
       sourcemaps.init(), //Инициализируем sourcemap
-      uglify(), //Сожмем наш js
+      // uglify(), //Сожмем наш js
       sourcemaps.write(), //Пропишем карты
       gulp.dest(path.dist.js), //Выплюнем готовый файл в build
       reload({stream: true}), //И перезагрузим сервер
@@ -104,27 +112,56 @@ gulp.task('img:build', function () {
 });
 
 // Build fonts
-gulp.task('fonts:build', function() {
+gulp.task('fonts:build', function () {
   gulp.src(path.src.fonts)
     .pipe(gulp.dest(path.dist.fonts))
 });
 
+// Deploy json DB
+gulp.task('db:deploy', function () {
+
+  let conn = ftp.create({
+    host: 'tina.timeweb.ru',
+    user: 'olegras_proshop',
+    password: '',
+    parallel: 10,
+    log: log.error('Json DB deploy failed')
+  });
+
+  // using base = '.' will transfer everything to /public_html correctly
+  // turn off buffering in gulp.src for best performance
+
+  return gulp.src(path.src.db, {base: '.', buffer: false})
+    .pipe(conn.newer('.')) // only upload newer files
+    .pipe(conn.dest('.'));
+
+});
+
+// Build json DB
+gulp.task('db:build', function () {
+  gulp.src(path.src.db)
+    .pipe(gulp.dest(path.dist.db))
+});
+
 // Build ALL
 gulp.task('build', [
+  'db:build',
+  // 'db:deploy',
   'html:build',
   'js:build',
   'css:build',
   'fonts:build',
-  'img:build'
+  'img:build',
 ]);
 
 // Auto-build changed files
-gulp.task('watch', function(){
+gulp.task('watch', function () {
   gulp.watch(path.watch.html, ['html:build']);
   gulp.watch(path.watch.css, ['css:build']);
   gulp.watch(path.watch.js, ['js:build']);
   gulp.watch(path.watch.img, ['img:build']);
   gulp.watch(path.watch.fonts, ['fonts:build']);
+  // gulp.watch(path.watch.db, ['db:deploy']);
 });
 
 // Create web-server
