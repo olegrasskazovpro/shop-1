9f528c9a0e47674d772f9c9e2625f104aeb57e15
OlegRasskazov <info@olegrasskazov.pro>
2018-08-28 13:32:37 +0300

    other js files connected + SetActiveLinks class added

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,41 @@
-//= jCarousel.js
-
 "use strict";
 
-/*class Slider {
+class SetActiveLinks {
   constructor () {
 
   }
 
-  init() {
-    console.log('Привет');
+  /**
+   * Set class="active" to nav links for page opened
+   */
+  setActiveClass() {
+    if (this.checkUrl('product.html')){
+      $('.menu a').removeAttr('class');
+      $('.menu>li a[href="product.html"]').addClass('menu-active');
+      $('.mega-list a:first').addClass('active');
+      $('.mega a:first').addClass('active');
+    }
+    if(this.checkUrl('index.html')){
+      $('.menu>li a[href="index.html"]').addClass('menu-active');
+    }
   }
-}*/
+
+  /**
+   * Check if page URL contains some string
+   * @param string url - regExp condition
+   * @returns {boolean} true if URL contains regExp
+   */
+  checkUrl(url) {
+    let checkUrl = new RegExp(url);
+    return checkUrl.test(document.location.href)
+  }
+}
 
 (function ($) {
   $(function () {
-    // let slider = new Slider();
-    // slider.init();
+    let pageInit = new SetActiveLinks();
+    pageInit.setActiveClass();
+
     $('.jcarousel').jcarousel({
       wrap: 'circular'
     });
@@ -28,3 +48,9 @@
     });
   })
 })(jQuery);
+
+//= product.js
+
+//= cart.js
+
+//= jCarousel.js
