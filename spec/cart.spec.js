2f52c38a87566c09efdd86bf699ae5d242cff5b1
OlegRasskazov <info@olegrasskazov.pro>
2018-09-12 00:51:30 +0300

    test template created

---
+++ spec/cart.spec.js
@@ -0,0 +1,7 @@
+"use strict";
+
+describe('Button "Add to cart" click handler function', () => {
+  it('Function have to return product id', () => {
+    expect(addToCartButtonHandler()).toBe()
+  })
+});
